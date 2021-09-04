8bd34ed1e4b3f273fa3432de516bb90c7b93b2f2
Steffen pedersen <stped18@student.sdu.dk>
2021-09-04 16:51:49 +0200

    start

---
+++ app/ns_app/ns_app/ns_app/LoginPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ns_app
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentView
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
